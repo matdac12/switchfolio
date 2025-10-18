6be63a865b36db83ce645b6393edc98cf54250ba
matdac12 <mattia.dacampo@gmail.com>
2025-10-18 23:20:42 +0200

    fixed tab name 02

--- src/utils/TitleUpdater.tsx
+++ src/utils/TitleUpdater.tsx
@@ -1,13 +1,13 @@
-"use client"
-import { useSwitch } from "@/components/Context/SwitchContext"
-import { useEffect } from "react"
+"use client";
+import { useSwitch } from "@/components/Context/SwitchContext";
+import { useEffect } from "react";
 
 export const TitleUpdater = () => {
-  const { isSwitchOn } = useSwitch()
+  const { isSwitchOn } = useSwitch();
 
   useEffect(() => {
-    document.title = isSwitchOn ? "Gruz" : "Rinkit Adhana"
-  }, [isSwitchOn])
+    document.title = isSwitchOn ? "0xmatti" : "0xmatti-it";
+  }, [isSwitchOn]);
 
-  return null
-}
+  return null;
+};
